b91e3088b2d7ad2208f95f32b88e086eb5889285
Braulio Diez Botella <braulio.diez@gmail.com>
2020-03-25 14:41:33 +0100

    12 CSS Modules completed

--- 03-bundling/01-webpack/12-css-modules/src/averageComponent.jsx
+++ 03-bundling/01-webpack/12-css-modules/src/averageComponent.jsx
@@ -15,6 +15,9 @@ export const AverageComponent = () => {
       <span className={classes.resultBackground}>
         Students average: {average}
       </span>
+      <div className={`jumbotron ${classes.resultBackground}`}>
+        <h1>Jumbotron students average: {average}</h1>
+      </div>
     </div>
   );
 };

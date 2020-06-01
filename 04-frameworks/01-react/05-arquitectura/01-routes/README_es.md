d7295055b97345ce486c3fbf573537eccbd5c454
Braulio Diez Botella <braulio.diez@gmail.com>
2020-06-01 13:02:06 +0200

    routes starting point

---
+++ 04-frameworks/01-react/05-arquitectura/01-routes/README_es.md
@@ -0,0 +1,29 @@
+# 01 Routes
+
+Vamos a hacer setup de las rutas de navegación y añadir escenas en blanco para:
+
+- Login
+- Panel de opciones principal.
+- Listado de empleados
+- Edición empleados.
+
+Vamos a montar las rutas de forma que sea fácil de matener a futuro.
+
+# Pasos
+
+- Copiate el ejemplo anterior _00-boilerplate_ y haz un _npm install_
+
+```bash
+npm install
+```
+
+# ¿Te apuntas a nuestro máster?
+
+Si te ha gustado este ejemplo y tienes ganas de aprender Front End
+guiado por un grupo de profesionales ¿Por qué no te apuntas a
+nuestro [Máster Front End Online Lemoncode](https://lemoncode.net/master-frontend#inicio-banner)? Tenemos tanto edición de convocatoria
+con clases en vivo, como edición continua con mentorización, para
+que puedas ir a tu ritmo y aprender mucho.
+
+Y si tienes ganas de meterte una zambullida en el mundo _devops_
+apuntate nuestro [Bootcamp devops online Lemoncode](https://lemoncode.net/bootcamp-devops#bootcamp-devops/inicio)
---
+++ 04-frameworks/01-react/05-arquitectura/01-routes/config/webpack/base.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const helpers = require('./helpers');
+
+module.exports = merge(
+  {},
+  {
+    context: helpers.resolveFromRootPath('src'),
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    entry: {
+      app: ['regenerator-runtime/runtime', './index.tsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            name: 'vendor',
+            test: /[\\/]node_modules[\\/]/,
+            enforce: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        favicon: 'assets/favicon.ico',
+        filename: 'index.html',
+        template: 'index.html',
+      }),
+    ],
+  }
+);

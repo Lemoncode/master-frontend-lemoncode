a4bec75f5db3227de5c9b6d8a3937da7db03cb88
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-06-19 17:03:07 +0200

    implement 03

--- 05-testing/01-react/03-hooks/03-component-did-mount/src/user.hooks.spec.ts
+++ 05-testing/01-react/03-hooks/03-component-did-mount/src/user.hooks.spec.ts
@@ -3,7 +3,7 @@ import { User } from './model';
 import { useUser } from './user.hooks';
 
 describe('useUser specs', () => {
-  it('should return user with initial values and setUser method when it calls it', () => {
+  it('should return user with initial values and setUser method when it calls it', async () => {
     // Arrange
     const initialUser: User = {
       name: 'John',
@@ -11,14 +11,23 @@ describe('useUser specs', () => {
     };
 
     // Act
-    const { result } = renderHook(() => useUser(initialUser));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useUser(initialUser)
+    );
 
     // Assert
     expect(result.current.user).toEqual(initialUser);
     expect(result.current.setUser).toEqual(expect.any(Function));
+
+    await waitForNextUpdate();
+    const updatedUser: User = {
+      name: 'Jane',
+      surname: 'Smith',
+    };
+    expect(result.current.user).toEqual(updatedUser);
   });
 
-  it('should update user when it calls setUser', () => {
+  it('should update user when it calls setUser', async () => {
     // Arrange
     const initialUser: User = {
       name: 'John',
@@ -26,7 +35,9 @@ describe('useUser specs', () => {
     };
 
     // Act
-    const { result } = renderHook(() => useUser(initialUser));
+    const { result, waitForNextUpdate } = renderHook(() => useUser(initialUser));
+
+    await waitForNextUpdate();
 
     act(() => {
       result.current.setUser({
--- 05-testing/01-react/03-hooks/03-component-did-mount/src/user.hooks.ts
+++ 05-testing/01-react/03-hooks/03-component-did-mount/src/user.hooks.ts
@@ -4,6 +4,13 @@ import { User } from './model';
 export const useUser = (initialUser: User) => {
   const [user, setUser] = React.useState(initialUser);
 
+  React.useEffect(() => {
+    // Simulate async call
+    setTimeout(() => {
+      setUser({ name: 'Jane', surname: 'Smith' });
+    }, 500);
+  }, []);
+
   return {
     user,
     setUser,

43fedc09f2bf667d07b7e4a1a93851fcecdd0748
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-06-19 16:29:45 +0200

    implement 05-router

--- 05-testing/01-react/02-components/05-router/src/app.tsx
+++ 05-testing/01-react/02-components/05-router/src/app.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import { NameEdit } from './name-edit';
-import { NameCollection } from './name-collection';
+import { Router } from './router';
 
 export const App: React.FunctionComponent = () => {
   return (
     <>
       <h1>05-Testing / 01 React</h1>
-      <NameEdit />
-      <NameCollection />
+      <Router />
     </>
   );
 };
--- 05-testing/01-react/02-components/05-router/src/name-collection.tsx
+++ 05-testing/01-react/02-components/05-router/src/name-collection.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { getNameCollection } from './name-api';
 
 export const NameCollection: React.FunctionComponent = () => {
   const [nameCollection, setNameCollection] = React.useState([]);
 
   React.useEffect(() => {
-    getNameCollection().then(names => {
+    getNameCollection().then((names) => {
       setNameCollection(names);
     });
   }, []);
 
   return (
     <ul>
-      {nameCollection.map(name => (
-        <li key={name}>{name}</li>
+      {nameCollection.map((name) => (
+        <li key={name}>
+          <Link to={`/users/${name}`}>{name}</Link>
+        </li>
       ))}
     </ul>
   );

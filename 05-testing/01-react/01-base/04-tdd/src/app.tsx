2d0b7a2cf626aac63a29e95be478b7fab00cdacc
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-06-19 09:46:03 +0200

    implement 04-tdd

--- 05-testing/01-react/01-base/04-tdd/src/app.tsx
+++ 05-testing/01-react/01-base/04-tdd/src/app.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
+import { getMembers } from './api';
+import { mapToMemberVMList } from './mapper';
 
 export const App: React.FunctionComponent = () => {
+  React.useEffect(() => {
+    getMembers().then((members) => {
+      console.log(mapToMemberVMList(members));
+    });
+  }, []);
+
   return <h1>05-Testing / 01 React</h1>;
 };
--- 05-testing/01-react/01-base/04-tdd/src/business.ts
+++
@@ -1,5 +0,0 @@
-export const isLowerThan = (value, max) => {
-  console.log(`The value: ${value} is lower than ${max}`);
-};
-
-export const max = 6;

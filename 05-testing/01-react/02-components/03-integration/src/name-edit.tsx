59697af33f7a3b7e33ce10456b8e58c58ec466cf
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-06-19 15:07:14 +0200

    implement 04-integration

--- 05-testing/01-react/02-components/03-integration/src/name-edit.tsx
+++ 05-testing/01-react/02-components/03-integration/src/name-edit.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import { Display } from './display';
+import { Edit } from './edit';
 
 export const NameEdit: React.FunctionComponent = () => {
   const [userName, setUserName] = React.useState('');
 
   return (
     <>
-      <h3>{userName}</h3>
-      <input value={userName} onChange={(e) => setUserName(e.target.value)} />
+      <Display userName={userName} />
+      <Edit userName={userName} onSetUserName={setUserName} />
     </>
   );
 };

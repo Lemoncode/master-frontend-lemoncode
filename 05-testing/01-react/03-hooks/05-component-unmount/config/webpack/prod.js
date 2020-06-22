555bc5a9354831d4f85924447089d58bb692ecd9
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-06-22 09:58:49 +0200

    copy previous example

---
+++ 05-testing/01-react/03-hooks/05-component-unmount/config/webpack/prod.js
@@ -0,0 +1,34 @@
+const merge = require('webpack-merge');
+const base = require('./base');
+const helpers = require('./helpers');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(base, {
+  mode: 'production',
+  output: {
+    path: helpers.resolveFromRootPath('dist'),
+    filename: './js/[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: './images/[name].[ext]?[hash]',
+          esModule: false,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[chunkhash].[name].css',
+      chunkFilename: '[chunkhash].[id].css',
+    }),
+  ],
+});

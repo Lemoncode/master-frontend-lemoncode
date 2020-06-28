9c62d868da26709f1608e89f804acfa859129a3f
Carlos Herrera <carherco@gmail.com>
2020-06-29 00:17:03 +0200

    formularios reactivos

---
+++ 04-frameworks/02-angular/06-form-reactivo/src/app/model/MemberEntity.ts
@@ -0,0 +1,5 @@
+export interface MemberEntity {
+  avatar_url: string;
+  id: string;
+  login: string;
+}
---
+++ 04-frameworks/02-angular/06-form-reactivo/src/app/pipes/search-by-login.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchByLoginPipe } from './search-by-login.pipe';
+
+describe('SearchByLoginPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchByLoginPipe();
+    expect(pipe).toBeTruthy();
+  });
+});

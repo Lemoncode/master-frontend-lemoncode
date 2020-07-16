79b693148547dcc4f1b81b118902abe2ed28f39d
Sara Lissette <lissette.ibnz@gmail.com>
2020-07-16 23:11:50 +0100

    Fix missing vuetify instance

--- 04-frameworks/03-vue/04 Recipe List/src/main.ts
+++ 04-frameworks/03-vue/04 Recipe List/src/main.ts
@@ -1,15 +1,16 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
 import VueRouter from "vue-router";
-import { router } from "./router";
+import Vuetify from "vuetify";
 import App from "./App.vue";
+import { router } from "./router";
 import "vuetify/dist/vuetify.min.css";
 
-Vue.use(Vuetify);
 Vue.use(VueRouter);
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
 
 new Vue({
+  vuetify,
   router,
   render: (h) => h(App),
 }).$mount("#root");

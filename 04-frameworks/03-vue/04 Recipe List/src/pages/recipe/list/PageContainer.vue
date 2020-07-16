a060fe229620dbb43bfabaab81113a8ad5c24b6d
Sara Lissette <lissette.ibnz@gmail.com>
2020-07-16 22:20:30 +0100

    Review 04 Recipe list sample and update dependencies

--- 04-frameworks/03-vue/04 Recipe List/src/pages/recipe/list/PageContainer.vue
+++ 04-frameworks/03-vue/04 Recipe List/src/pages/recipe/list/PageContainer.vue
@@ -1,28 +1,46 @@
 <template>
-  <recipe-list-page :recipes="recipes" />
+  <recipe-list-page
+    :search-text="searchText"
+    :on-search="onSearch"
+    :recipes="filteredRecipes"
+  />
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Recipe } from './viewModel';
-import { mapRecipeListModelToVm } from './mappers';
-import { fetchRecipes } from '../../../rest-api/api/recipe';
-import RecipeListPage from './Page.vue';
+import Vue from "vue";
+import { fetchRecipes } from "../../../rest-api/api/recipe";
+import { mapRecipeListModelToVm } from "./mappers";
+import { filterRecipesByCommaSeparatedText } from "./business/filterRecipeBusiness";
+import { Recipe } from "./viewModel";
+import RecipeListPage from "./Page.vue";
 
 export default Vue.extend({
-  name: 'RecipeListPageContainer',
-  components: { RecipeListPage },
+  name: "RecipeListPageContainer",
+  components: {
+    RecipeListPage,
+  },
   data() {
     return {
       recipes: [] as Recipe[],
+      searchText: "",
     };
+  },
+  computed: {
+    filteredRecipes(): Recipe[] {
+      return filterRecipesByCommaSeparatedText(this.recipes, this.searchText);
+    },
   },
   created() {
     fetchRecipes()
-      .then(recipes => {
+      .then((recipes) => {
         this.recipes = mapRecipeListModelToVm(recipes);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
+  },
+  methods: {
+    onSearch(value: string) {
+      this.searchText = value;
+    },
   },
 });
 </script>

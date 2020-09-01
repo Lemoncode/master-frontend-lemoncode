6a9a21068fb6d70106af5f49b271bb0f1038dc63
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-01 09:33:21 +0200

    14 updated

--- 03-bundling/01-webpack/14-production-config/webpack.prod.js
+++ 03-bundling/01-webpack/14-production-config/webpack.prod.js
@@ -1,7 +1,7 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  stats: "verbose"
+  stats: "verbose",
 });

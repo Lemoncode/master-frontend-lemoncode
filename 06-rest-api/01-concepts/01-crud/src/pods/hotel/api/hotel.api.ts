0c186a78949f5d5528f0e1e9870f88f997a631d1
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-09-01 14:11:55 +0200

    implement example

--- 06-rest-api/01-concepts/01-crud/src/pods/hotel/api/hotel.api.ts
+++ 06-rest-api/01-concepts/01-crud/src/pods/hotel/api/hotel.api.ts
@@ -1,15 +1,27 @@
+import Axios from 'axios';
 import { Hotel } from './hotel.api-model';
 import { Lookup } from 'common/models';
-import { mockCities, mockHotelCollection } from './hotel.mock-data';
+
+const hotelListUrl = '/api/hotels';
+const cityListUrl = '/api/cities';
 
 export const getHotel = async (id: string): Promise<Hotel> => {
-  return mockHotelCollection.find((h) => h.id === id);
+  const { data } = await Axios.get<Hotel>(`${hotelListUrl}/${id}`);
+
+  return data;
 };
 
 export const getCities = async (): Promise<Lookup[]> => {
-  return mockCities;
+  const { data } = await Axios.get<Lookup[]>(cityListUrl);
+
+  return data;
 };
 
 export const saveHotel = async (hotel: Hotel): Promise<boolean> => {
+  if (hotel.id) {
+    await Axios.put<Hotel>(`${hotelListUrl}/${hotel.id}`, hotel);
+  } else {
+    await Axios.post<Hotel>(hotelListUrl, hotel);
+  }
   return true;
 };

cc0e6b3deb5b31b3e45f5f21042131cfd35ed268
Carlos Herrera <carherco@gmail.com>
2020-09-11 12:50:00 +0200

    algunos ficheros estaban sin guardar y no se habían subido bien

--- 05-testing/02-angular/05- e2e/e2e/src/app.po.ts
+++ 05-testing/02-angular/05- e2e/e2e/src/app.po.ts
@@ -4,4 +4,12 @@ export class AppPage {
   navigateTo(url): Promise<unknown> {
     return browser.get(browser.baseUrl + url) as Promise<unknown>;
   }
+
+  getTitleText(): Promise<string> {
+    return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+
+  getElementByCss(selector: string) {
+    return element(by.css(selector));
+  }
 }

30e28a8269cb5d0f555e21893a897e340d8ab1a0
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-09-23 18:20:40 +0200

    update server

--- 06-rest-api/03-graphql/02-crud/00-boilerplate/server/src/index.ts
+++ 06-rest-api/03-graphql/02-crud/00-boilerplate/server/src/index.ts
@@ -1,14 +1,11 @@
 import 'regenerator-runtime/runtime';
 import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
 import path from 'path';
 import { hotelApi, cityApi } from './api';
 
 const PORT = 3000;
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));

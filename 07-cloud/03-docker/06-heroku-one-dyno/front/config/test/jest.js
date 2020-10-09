cb830e63bb084f572cfff6ef21e4c983f4293ce5
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-10-09 14:23:16 +0200

    copy previous example

---
+++ 07-cloud/03-docker/06-heroku-one-dyno/front/config/test/jest.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rootDir: '../../',
+  preset: 'ts-jest',
+  restoreMocks: true,
+  moduleDirectories: ['<rootDir>/src', 'node_modules'],
+  setupFilesAfterEnv: ['<rootDir>/config/test/setup-after.ts'],
+};

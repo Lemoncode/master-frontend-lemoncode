310b83f1ed769c260fe81f8e463e661f16ad1250
gangsthub <paulmelero@gmail.com>
2021-01-07 22:46:08 +0100

    finalizado lo que había planeado para la primera sesión
    Ver
    https://github.com/Lemoncode/master-frontend-lemoncode/blob/master/04-frameworks/03-vue/01_ecommerce_app/README.md

--- 04-frameworks/03-vue/01_ecommerce_app_primer_dia/src/components/ProductList.vue
+++ 04-frameworks/03-vue/01_ecommerce_app_primer_dia/src/components/ProductList.vue
@@ -1,7 +1,30 @@
 <template>
-  <ul>
+  <ul class="product-list">
     <li v-for="product in list" :key="product.id">
-      {{ product.title }}
+      <article class="grid product-container card">
+        <div class="image">
+          <img :src="`https://picsum.photos/id/${product.id}/+200`" alt="" />
+        </div>
+        <div class="product-container__content">
+          <h2>
+            {{ product.title }}
+          </h2>
+          <p>
+            <span class="grey-text">Author: </span>
+            <strong>{{ product.author }}</strong>
+          </p>
+          <p>
+            <span class="grey-text">Publisher: </span>
+            {{ product.publisher }}
+          </p>
+          <p><span class="grey-text">Year: </span>{{ product.published }}</p>
+        </div>
+        <div class="flex product-container__aside">
+          <div class="text-align-end aside__price">
+            {{ product.price }}
+          </div>
+        </div>
+      </article>
     </li>
   </ul>
 </template>
@@ -10,7 +33,6 @@
 import { productService } from '@/services/products'
 import { defineComponent } from 'vue'
 import { Product } from '@/types'
-
 export default defineComponent({
   data() {
     return {
@@ -23,4 +45,15 @@ export default defineComponent({
 })
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.product-list {
+  padding: 0;
+  li {
+    margin-bottom: 2em;
+  }
+}
+.product-container {
+  align-items: flex-start;
+  grid-template-columns: 210px 1fr 100px;
+}
+</style>

e0811464f94dbea92cd17847ad0d4172d2a7a649
gangsthub <paulmelero@gmail.com>
2021-01-08 10:46:28 +0100

    copiado proyecto para el segundo día de master

---
+++ 04-frameworks/03-vue/01_ecommerce_app_segundo_dia/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <div>Logo</div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style></style>

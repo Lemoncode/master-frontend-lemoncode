443312f507dd5d25e11aba013e09ea42413b1bbb
Braulio Diez Botella <braulio.diez@gmail.com>
2021-02-15 20:58:41 +0100

    CORS not working

---
+++ 06-rest-api/04-socket-io/01-hello-socket-io/back/src/app.ts
@@ -0,0 +1,55 @@
+import { createApp } from './express.server';
+import { envConstants } from './env.constants';
+import { api } from './api';
+import express from 'express';
+import path from 'path';
+import * as http from 'http';
+import { Server, Socket } from 'socket.io';
+import cors from 'cors';
+
+const app = createApp();
+
+const socketapp = new http.Server(app);
+const io = new Server(socketapp);
+
+//options for cors midddleware
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  // IMPORTANT LIMIT HERE YOUR CLIENT APPS DOMAINS
+  origin: '*',
+  preflightContinue: false,
+};
+
+app.use(cors(options));
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.use('/api', api);
+
+app.listen(envConstants.PORT, () => {
+  console.log(`Server ready at http://localhost:${envConstants.PORT}/api`);
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on('connection', function (socket: Socket) {
+  console.log('** connection recieved');
+  socket.emit('message', { type: 'CONNECTION_SUCCEEDED' });
+
+  socket.on('message', function (body: any) {
+    console.log(body);
+    io.emit('message', body);
+  });
+});
+
+const server = socketapp.listen(3000, function () {
+  console.log('listening on *:3000');
+});

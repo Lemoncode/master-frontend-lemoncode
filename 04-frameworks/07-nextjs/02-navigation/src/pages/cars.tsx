cf69337ad96e07cd72fcfd69419a35ce9bb21f6c
daniel.sanchez <daniel.sanchez@lemoncode.net>
2021-02-16 15:13:51 +0100

    implement example 02

---
+++ 04-frameworks/07-nextjs/02-navigation/src/pages/cars.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+const CarListPage = () => {
+  const router = useRouter();
+  const onNavigateBack = () => {
+    router.push('/'); // or router.back()
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Rent a car - Car list</title>
+      </Head>
+      <h2>Car list page</h2>
+      <ul>
+        <li>Audi Q8</li>
+        <li>BMW X7</li>
+      </ul>
+      <button onClick={onNavigateBack}>Navigate to home</button>
+    </>
+  );
+};
+
+export default CarListPage;
--- 04-frameworks/07-nextjs/02-navigation/src/pages/index.tsx
+++ 04-frameworks/07-nextjs/02-navigation/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
 const HomePage = () => {
-  return <h2>Hello from Nextjs</h2>;
+  return (
+    <>
+      <Head>
+        <title>Rent a car - Home</title>
+      </Head>
+      <h2>Hello from Nextjs</h2>
+      <Link href="/cars">Navigate to car list</Link>
+    </>
+  );
 };
 
 export default HomePage;

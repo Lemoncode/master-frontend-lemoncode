10c4f45eb055710ff3632db9267127534555557b
Braulio Diez Botella <braulio.diez@gmail.com>
2021-02-17 17:37:44 +0100

    removed security

--- 06-rest-api/04-socket-io/04-auth/back/src/api.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-export const api = Router();
-
-api.get('/', async (req, res) => {
-  res.send({ id: '1', name: 'test data' });
-});
--- 06-rest-api/04-socket-io/04-auth/back/src/env.constants.ts
+++
@@ -1,4 +0,0 @@
-export const envConstants = {
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-};

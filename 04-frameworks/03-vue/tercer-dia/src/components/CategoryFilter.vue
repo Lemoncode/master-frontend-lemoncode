de25ac50e218c0628e2a429bf523b57052195ab8
gangsthub <paulmelero@gmail.com>
2021-07-16 22:07:22 +0200

    app, como se quedó al tercer día

---
+++ 04-frameworks/03-vue/tercer-dia/src/components/CategoryFilter.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="button"
+    type="button"
+    @click="$emit('selected-category', category)"
+  >
+    {{ category }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    category: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style></style>
--- 04-frameworks/03-vue/tercer-dia/src/components/Header.vue
+++ 04-frameworks/03-vue/tercer-dia/src/components/Header.vue
@@ -1,13 +1,24 @@
 <template>
   <nav class="nav flex align-items-center justify-content-between container">
     <div>Logo</div>
-    <div>Cart</div>
+    <div class="grid cart-container">
+      <div>Cart</div>
+      <div class="badge text-align-center" v-if="totalItemsInCart">
+        {{ totalItemsInCart }}
+      </div>
+    </div>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-export default defineComponent({})
+import { mapGetters } from 'vuex'
+
+export default defineComponent({
+  computed: {
+    ...mapGetters('CartModule', ['totalItemsInCart']),
+  },
+})
 </script>
 
 <style scoped>
@@ -16,5 +27,21 @@ export default defineComponent({})
   background-color: rgb(198, 247, 244);
   position: sticky;
   top: 0;
+}
+.cart-container {
+  padding-right: 60px;
+  grid-template-columns: auto 60px;
+  place-items: center;
+}
+.badge {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  background-color: rgba(10, 105, 194, 0.5);
+  color: #ffffff;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  margin-right: 0;
 }
 </style>

2f7f920eaf3d3dfbfe3c33643110dc7c001fa0d4
daniel.sanchez <daniel.sanchez@lemoncode.net>
2021-07-19 13:38:57 +0200

    update example 01-graphql-backend

--- 06-rest-api/03-graphql/02-crud/01-graphql-backend/config/webpack/dev.js
+++ 06-rest-api/03-graphql/02-crud/01-graphql-backend/config/webpack/dev.js
@@ -1,25 +1,12 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const base = require('./base');
-const helpers = require('./helpers');
 
-const hotReloadingEntries = ['react-hot-loader/patch'];
-
-module.exports = merge.strategy({
-  entry: 'prepend',
-})(base, {
+module.exports = merge(base, {
   mode: 'development',
-  devtool: 'inline-source-map',
-  resolve: {
-    alias: {
-      'react-dom': '@hot-loader/react-dom',
-    },
-  },
-  entry: {
-    app: hotReloadingEntries,
-  },
+  devtool: 'eval-source-map',
   output: {
-    path: helpers.resolveFromRootPath('dist'),
     filename: '[name].js',
   },
   devServer: {
@@ -35,16 +22,14 @@ module.exports = merge.strategy({
   module: {
     rules: [
       {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'img/[name].[ext]',
-          esModule: false,
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [require.resolve('react-refresh/babel')],
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       },
     ],
   },
@@ -52,5 +37,6 @@ module.exports = merge.strategy({
     new Dotenv({
       path: 'dev.env',
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 });

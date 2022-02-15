31623c5ca7e61456b22e79d5515cad4aaf8305d7
Braulio Diez Botella <braulio.diez@gmail.com>
2022-02-15 19:26:14 +0100

    06 viewbox completed

--- 04-frameworks/04-d3js/01-form-barchart-to-semi-arch/06-view-box/src/index.ts
+++ 04-frameworks/04-d3js/01-form-barchart-to-semi-arch/06-view-box/src/index.ts
@@ -1,31 +1,61 @@
 import * as d3 from "d3";
-import { resultCollectionSpainNov19 } from "./data";
+import { legendColor } from "d3-svg-legend";
+import {
+  resultCollectionSpainNov19,
+  resultCollectionSpainApr19,
+  ResultEntry,
+} from "./data";
 
-const svgDimensions = { width: 500, height: 250 };
+const svgDimensions = { width: 500, height: 500 };
 const margin = { left: 5, right: 5, top: 10, bottom: 10 };
+
 const chartDimensions = {
   width: svgDimensions.width - margin.left - margin.right,
   height: svgDimensions.height - margin.bottom - margin.top,
 };
 
-const partiesColor = [
-  "#ED1D25",
-  "#0056A8",
-  "#5BC035",
-  "#6B2E68",
-  "#F3B219",
-  "#FA5000",
-  "#C50048",
-  "#029626",
-  "#A3C940",
-  "#0DDEC5",
-  "#FFF203",
-  "#FFDB1B",
-  "#E61C13",
-  "#73B1E6",
-  "#BECD48",
-  "#017252",
-];
+const maxNumberSeats = resultCollectionSpainNov19.reduce(
+  (max, item) => (item.seats > max ? item.seats : max),
+  0
+);
+
+const politicalPartiesCount = resultCollectionSpainNov19.length;
+
+const partiesColorScale = d3
+  .scaleOrdinal([
+    "#ED1D25",
+    "#0056A8",
+    "#5BC035",
+    "#6B2E68",
+    "#F3B219",
+    "#FA5000",
+    "#C50048",
+    "#029626",
+    "#A3C940",
+    "#0DDEC5",
+    "#FFF203",
+    "#FFDB1B",
+    "#E61C13",
+    "#73B1E6",
+    "#FFA500",
+  ])
+  .domain([
+    "PSOE",
+    "PP",
+    "VOX",
+    "UP",
+    "ERC",
+    "Cs",
+    "JxCat",
+    "PNV",
+    "Bildu",
+    "Más pais",
+    "CUP",
+    "CC",
+    "BNG",
+    "Teruel Existe",
+    "Compromis",
+  ]);
 
 const svg = d3
   .select("body")
@@ -40,11 +70,18 @@ const svg = d3
     }`
   );
 
+svg
+  .append("g")
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", chartDimensions.width)
+  .attr("height", chartDimensions.height)
+  .attr("style", "fill: #FBFAF0");
+
 const chartGroup = svg
   .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`)
-  .attr("width", chartDimensions.width)
-  .attr("height", chartDimensions.height);
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 const radius = chartDimensions.width / 2.3;
 
@@ -62,19 +99,43 @@ const arc = d3
   .outerRadius(radius);
 
 const pieChart = d3
-  .pie()
+  .pie<ResultEntry>()
   .startAngle(-90 * (Math.PI / 180))
-  .endAngle(90 * (Math.PI / 180));
+  .endAngle(90 * (Math.PI / 180))
+  .value(function (d: any) {
+    return d.seats;
+  })
+  .sort(null);
 
-const politicalResultsOnlyNumbers: number[] = resultCollectionSpainNov19.map(
-  (result) => result.seats
-);
-
-const pie = pieChart(politicalResultsOnlyNumbers);
-
-const arcs = chartGroup.selectAll("slice").data(pie).enter();
+const arcs = chartGroup
+  .selectAll("slice")
+  .data(pieChart(resultCollectionSpainNov19))
+  .enter();
 
 arcs
   .append("path")
   .attr("d", <any>arc) // Hack typing: https://stackoverflow.com/questions/35413072/compilation-errors-when-drawing-a-piechart-using-d3-js-typescript-and-angular/38021825
-  .attr("fill", (d, i) => partiesColor[i]); // TODO color ordinal
+  .attr("fill", (d, i) => {
+    console.log(d.data.party);
+    return partiesColorScale(d.data.party);
+  });
+
+const updateChart = (data: ResultEntry[]) => {
+  d3.selectAll("path")
+    .data(pieChart(<any>data))
+    .transition()
+    .duration(500)
+    .attr("d", <any>arc);
+};
+
+document
+  .getElementById("april")
+  .addEventListener("click", function handleResultsApril() {
+    updateChart(resultCollectionSpainApr19);
+  });
+
+document
+  .getElementById("november")
+  .addEventListener("click", function handleResultsNovember() {
+    updateChart(resultCollectionSpainNov19);
+  });

ab32a20e2a3286b59f388aa4f4ae99873cc42e4a
Braulio Diez Botella <braulio.diez@gmail.com>
2022-02-15 18:53:56 +0100

    02B skip completed

--- 04-frameworks/04-d3js/01-form-barchart-to-semi-arch/02B-SKIP-single-bar-stack-layout/src/index.ts
+++ 04-frameworks/04-d3js/01-form-barchart-to-semi-arch/02B-SKIP-single-bar-stack-layout/src/index.ts
@@ -1,6 +1,22 @@
 import * as d3 from "d3";
 import { resultCollectionSpainNov19 } from "./data";
 
+const svgDimensions = { width: 500, height: 500 };
+const margin = { left: 5, right: 5, top: 10, bottom: 10 };
+const chartDimensions = {
+  width: svgDimensions.width - margin.left - margin.right,
+  height: svgDimensions.height - margin.bottom - margin.top,
+};
+
+const maxNumberSeats = resultCollectionSpainNov19.reduce(
+  (max, item) => (item.seats > max ? item.seats : max),
+  0
+);
+
+const politicalPartiesKeys: string[] = resultCollectionSpainNov19.map(
+  (item) => item.party
+);
+
 // Since we are going to use stack layout
 // We are going to format the data in the following format
 // {
@@ -25,23 +41,6 @@ const singleElectionResult = resultCollectionSpainNov19.reduce(
 // we just wrap it in an array
 const data = [singleElectionResult];
 
-const svgDimensions = { width: 800, height: 500 };
-const margin = { left: 5, right: 5, top: 10, bottom: 10 };
-const chartDimensions = {
-  width: svgDimensions.width - margin.left - margin.right,
-  height: svgDimensions.height - margin.bottom - margin.top,
-};
-const totalNumberSeats = resultCollectionSpainNov19.reduce(
-  (sum, item) => sum + item.seats,
-  0
-);
-
-const barHeight = 200;
-
-const politicalPartiesKeys: string[] = resultCollectionSpainNov19.map(
-  (item) => item.party
-);
-
 const partiesColorScale = d3
   .scaleOrdinal([
     "#ED1D25",
@@ -59,25 +58,48 @@ const partiesColorScale = d3
     "#E61C13",
     "#73B1E6",
   ])
-  .domain(politicalPartiesKeys);
+  .domain([
+    "PSOE",
+    "PP",
+    "VOX",
+    "UP",
+    "ERC",
+    "Cs",
+    "JxCat",
+    "PNV",
+    "Bildu",
+    "Más pais",
+    "CUP",
+    "CC",
+    "BNG",
+    "Teruel Existe",
+  ]);
+
+const politicalPartiesCount = resultCollectionSpainNov19.length;
+
+const barHeight = 200; // We could calculate this based on svg height
 
 const svg = d3
   .select("body")
   .append("svg")
   .attr("width", svgDimensions.width)
-  .attr("height", svgDimensions.height)
-  .attr("style", "background-color: #FBFAF0");
+  .attr("height", svgDimensions.height);
+
+const totalNumberSeats = resultCollectionSpainNov19.reduce(
+  (sum, item) => sum + item.seats,
+  0
+);
+
+const xScale = d3
+  .scaleLinear()
+  .domain([0, totalNumberSeats])
+  .range([0, chartDimensions.width]);
 
 const chartGroup = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
   .attr("width", chartDimensions.width)
   .attr("height", chartDimensions.height);
-
-const xScale = d3
-  .scaleLinear()
-  .domain([0, totalNumberSeats])
-  .range([0, chartDimensions.width]);
 
 // Let's create our stack layout
 // we are going to pass the keys (PSOE, PP, VOX, UP, Cs...)
@@ -92,24 +114,7 @@ const stack = d3.stack().keys(politicalPartiesKeys);
 //]
 const series = stack(data);
 
-chartGroup
-  .selectAll("rect")
-  .data(series)
-  .enter()
-  .append("rect")
-  .attr("width", (d) => {
-    // To get the width of the current item we have to substract
-    // the final stack value - the initial stack value
-    return xScale(d[0][1] - d[0][0]);
-  })
-  .attr("height", barHeight)
-  .attr("x", (d, i) => {
-    // We take as starting point the first coordinate
-    // e.g. PP 120, 208 -> we start at 120 (where PSOE ended, and on the width param sum up that value)
-    return xScale(d[0][0]);
-  })
-  .attr("y", (d) => chartDimensions.height - barHeight)
-  .attr("fill", (d, i) => partiesColorScale(d.key));
+let currentXPosition = 0;
 
 chartGroup
   .selectAll("rect")
@@ -128,4 +133,4 @@ chartGroup
     return xScale(d[0][0]);
   })
   .attr("y", (d) => chartDimensions.height - barHeight)
-  .attr("fill", (d, i) => partiesColorScale(d.key));
+  .attr("fill", (d) => partiesColorScale(d.key));

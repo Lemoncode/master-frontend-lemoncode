ed7604105b81158d9eb8e0d3a74c2b4ee8de49ae
nasdan <daniel.sanchez@lemoncode.net>
2022-03-09 10:41:50 +0100

    reorder examples

---
+++ 06-rest-api/05-oauth/02-cookie-and-jwt/src/app.ts
@@ -0,0 +1,15 @@
+import { createApp } from './express.server';
+import { envConstants } from './env.constants';
+import { api } from './api';
+import express from 'express';
+import path from 'path';
+
+const app = createApp();
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.use('/api', api);
+
+app.listen(envConstants.PORT, () => {
+  console.log(`Server ready at http://localhost:${envConstants.PORT}/api`);
+});

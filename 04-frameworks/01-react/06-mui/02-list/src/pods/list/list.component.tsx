0ac84e58a724f1ab71f25232f0116e859bc01a13
Victor Borrego Perez <victor.borrego@lemoncode.net>
2022-06-10 14:39:18 +0200

    update react repo - add mui list sample

--- 04-frameworks/01-react/06-mui/02-list/src/pods/list/list.component.tsx
+++ 04-frameworks/01-react/06-mui/02-list/src/pods/list/list.component.tsx
@@ -1,9 +1,15 @@
 import { routes } from "@/core";
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { MemberEntity } from "./list.vm";
-import css from "./list.styles.css";
-import classNames from "classnames";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import ListItemButton from "@mui/material/ListItemButton";
 
 interface Props {
   members: MemberEntity[];
@@ -11,21 +17,34 @@ interface Props {
 
 export const ListComponent: React.FC<Props> = (props) => {
   const { members } = props;
+  const navigate = useNavigate();
+
   return (
     <>
       <h2>Hello from List page</h2>
-      <div className={classNames(css.container, css.someAdditionalClass)}>
-        <span className={css.header}>Avatar</span>
-        <span className={css.header}>Id</span>
-        <span className={css.header}>Name</span>
+
+      <List>
         {members.map((member) => (
           <>
-            <img src={member.avatar_url} />
-            <span>{member.id}</span>
-            <Link to={routes.details(member.login)}>{member.login}</Link>
+            <ListItem key={member.id} disablePadding>
+              <ListItemButton
+                role={undefined}
+                onClick={() => navigate(routes.details(member.login))}
+                dense
+              >
+                <ListItemAvatar>
+                  <Avatar alt={member.login} src={member.avatar_url} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={member.login}
+                  secondary={<Typography>{member.id}</Typography>}
+                />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
           </>
         ))}
-      </div>
+      </List>
     </>
   );
 };

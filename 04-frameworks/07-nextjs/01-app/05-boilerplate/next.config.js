d5477ffd1b38e44233c595d9173f5d645c4fa981
nasdan <daniel.sanchez@lemoncode.net>
2022-07-04 18:12:17 +0200

    update boilerplate

---
+++ 04-frameworks/07-nextjs/01-app/05-boilerplate/next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  compiler: {
+    emotion: true,
+  },
+  modularizeImports: {
+    '@mui/material/?(((\\w*)?/?)*)': {
+      transform: '@mui/material/{{ matches.[1] }}/{{member}}',
+    },
+    '@mui/icons-material/?(((\\w*)?/?)*)': {
+      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
+    },
+  },
+};

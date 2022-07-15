b6b00da972c2c3fdff24bca281bbd10d8d128ac3
Paul Melero <paulmelero@gmail.com>
2022-07-15 18:59:00 +0200

    refactor product service

--- 04-frameworks/03-vue/ecommerce-app/src/services/products.ts
+++ 04-frameworks/03-vue/ecommerce-app/src/services/products.ts
@@ -2,7 +2,9 @@ import type { Product } from '@/types'
 
 export const productService = {
   async get(): Promise<Product[]> {
-    const books = await import('./books.mock.json').then((m) => m.default)
+    const books = await fetch('/books.mock.json').then((response) =>
+      response.json()
+    )
     return books
   },
 }

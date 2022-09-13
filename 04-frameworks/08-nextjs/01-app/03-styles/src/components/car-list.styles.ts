992d4193aa9f62b1dc58e0ada2348f0d01ea8c14
daniel.sanchez <daniel.sanchez@lemoncode.net>
2022-09-13 11:05:16 +0200

    move nextjs example to 08

--- 04-frameworks/08-nextjs/01-app/03-styles/src/components/car-list.styles.ts
+++
@@ -1,7 +0,0 @@
-import { css } from '@emotion/css';
-
-export const root = css`
-  list-style: none;
-  background-color: chocolate;
-  color: white;
-`;
--- 04-frameworks/08-nextjs/01-app/07-client-side-rendering/src/common/constants/env.constants.ts
+++
@@ -1,4 +0,0 @@
-export const envConstants = {
-  BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
-  BASE_PICTURES_URL: process.env.NEXT_PUBLIC_BASE_PICTURES_URL,
-};

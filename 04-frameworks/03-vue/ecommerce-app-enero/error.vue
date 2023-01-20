8442ee412393161a340125c424927313276c2ea9
Paul <paul.melero@backmarket.com>
2023-01-20 21:44:33 +0100

    segunda sesion

---
+++ 04-frameworks/03-vue/ecommerce-app-enero/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Oops!</h1>
+    <p>
+      {{ error?.message }}
+    </p>
+    <p>
+      {{ error?.statusCode }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const error = useError()
+</script>

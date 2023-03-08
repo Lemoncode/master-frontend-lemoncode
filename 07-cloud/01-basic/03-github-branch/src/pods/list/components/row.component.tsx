c1c5ae56a735ee845efad013fdeb4b854cc23ae6
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-03-08 13:39:54 +0100

    update 03

--- 07-cloud/01-basic/03-github-branch/src/pods/list/components/row.component.tsx
+++ 07-cloud/01-basic/03-github-branch/src/pods/list/components/row.component.tsx
@@ -7,7 +7,7 @@ interface Props {
   member: Member;
 }
 
-export const RowComponent: React.FunctionComponent<Props> = (props) => {
+export const RowComponent: React.FC<Props> = (props) => {
   const { member } = props;
 
   return (

50fa0c3437feac78183a3d7488ee0bf873128149
Javier Calzado <javi.calzado@lemoncode.net>
2023-05-10 17:50:42 +0200

    reorganization

---
+++ 02-languages/05-playgrounds/03-playground-ejercicios/src/index.ts
@@ -0,0 +1,9 @@
+import "./00 practice/exercise";
+import "./01 deliverables/exercise-01";
+import "./01 deliverables/exercise-02";
+import "./01 deliverables/exercise-03";
+import "./01 deliverables/exercise-04";
+import "./01 deliverables/exercise-05";
+import "./02 challenges/exercise";
+
+console.log("Welcome to Exercises Playground.");

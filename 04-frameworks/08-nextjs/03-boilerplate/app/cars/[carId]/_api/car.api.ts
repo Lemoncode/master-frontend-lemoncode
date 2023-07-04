7b84d87a9de79fdcd3c0c7b4528000fe982ac133
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-07-04 19:28:27 +0200

    update 03

--- 04-frameworks/08-nextjs/03-boilerplate/app/cars/[carId]/_api/car.api.ts
+++ 04-frameworks/08-nextjs/03-boilerplate/app/cars/[carId]/_api/car.api.ts
@@ -1,15 +1,16 @@
-import Axios from 'axios';
 import { envConstants } from '@/_core/constants';
 import { Car } from './car.api-model';
 
 const url = `${envConstants.BASE_API_URL}/cars`;
 
 export const getCar = async (id: string): Promise<Car> => {
-  const { data } = await Axios.get<Car>(`${url}/${id}`);
-  return data;
+  return await fetch(`${url}/${id}`).then((response) => response.json());
 };
 
 export const bookCar = async (car: Car): Promise<boolean> => {
-  await Axios.put(`${url}/${car.id}`, car);
+  await fetch(`${url}/${car.id}`, {
+    method: 'POST',
+    body: JSON.stringify(car),
+  });
   return true;
 };
--- 04-frameworks/08-nextjs/03-boilerplate/app/cars/_api/car-list.api.ts
+++ 04-frameworks/08-nextjs/03-boilerplate/app/cars/_api/car-list.api.ts
@@ -3,6 +3,6 @@ import { Car } from './car-list.api-model';
 
 const url = `${envConstants.BASE_API_URL}/cars`;
 
-export const getCarList = async (options?: RequestInit): Promise<Car[]> => {
-  return await fetch(url, options).then((response) => response.json());
+export const getCarList = async (): Promise<Car[]> => {
+  return await fetch(url).then((response) => response.json());
 };

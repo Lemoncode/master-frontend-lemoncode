1c01bbce13763725f0adbc85931fc4b35392d6ee
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-07-11 09:31:56 +0200

    copy previous

---
+++ 04-frameworks/12-solidjs/04-createEffect/src/index.tsx
@@ -0,0 +1,34 @@
+import { createSignal, onCleanup } from "solid-js";
+import { render } from "solid-js/web";
+
+const App = () => {
+  const [show, setShow] = createSignal(false);
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        checked={show()}
+        onInput={(e) => setShow(e.currentTarget.checked)}
+      />
+      <span>Show date</span>
+      {show() && <CurrentDate />}
+    </>
+  );
+};
+
+const CurrentDate = () => {
+  const [date, setDate] = createSignal("No date");
+  const timer = setTimeout(
+    () => setDate(new Date().toLocaleTimeString()),
+    4000
+  );
+  onCleanup(() => {
+    console.log("Unmounting");
+    clearTimeout(timer);
+  });
+
+  return <h1>Date: {date()}</h1>;
+};
+
+render(() => <App />, document.getElementById("root"));

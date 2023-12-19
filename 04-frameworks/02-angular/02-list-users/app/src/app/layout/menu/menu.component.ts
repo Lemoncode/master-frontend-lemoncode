0223b11a13174be98ef3b1cf10ea9bfe698c74b9
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-12-19 20:25:20 +0100

    list of users upgraded

---
+++ 04-frameworks/02-angular/02-list-users/app/src/app/layout/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}

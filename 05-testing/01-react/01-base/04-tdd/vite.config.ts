9969c27c7f1d92d747be795198e7ce80a977efa9
daniel.sanchez <daniel.sanchez@lemoncode.net>
2024-12-15 16:58:57 +0100

    update 04

--- 05-testing/01-react/01-base/04-tdd/vite.config.ts
+++ 05-testing/01-react/01-base/04-tdd/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 });

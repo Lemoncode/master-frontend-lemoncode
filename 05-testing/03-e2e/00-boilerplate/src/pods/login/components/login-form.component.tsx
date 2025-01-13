8cf3dc1c66f32ee54f55bea9aa5c1cda712e8987
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-01-13 08:59:50 +0100

    update boilerplate

--- 05-testing/03-e2e/00-boilerplate/src/pods/login/components/login-form.component.tsx
+++ 05-testing/03-e2e/00-boilerplate/src/pods/login/components/login-form.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { Button } from '@mui/material';
-import { TextFieldComponent } from 'common/components';
+import { TextFieldComponent } from '#common/components';
 import { Login, createEmptyLogin } from '../login.vm';
 import { formValidation } from '../login.validations';
 import * as classes from './login-form.styles';

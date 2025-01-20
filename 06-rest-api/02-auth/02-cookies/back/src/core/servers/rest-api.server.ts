6d614ee4f94d639003b976702fe260b9e687d289
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-01-20 19:18:49 +0100

    update 02-cookies

--- 06-rest-api/02-auth/02-cookies/back/src/core/servers/rest-api.server.ts
+++ 06-rest-api/02-auth/02-cookies/back/src/core/servers/rest-api.server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-export const createApp = () => {
+export const createRestApiServer = () => {
   const app = express();
-
   app.use(express.json());
   app.use(cookieParser());
 

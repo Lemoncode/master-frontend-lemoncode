5c99637695627d8ad59818873ff87cea519d767d
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-01-30 22:01:30 +0100

    update code

--- 06-rest-api/02-auth/03-storage/front/src/core/api/api.helpers.ts
+++ 06-rest-api/02-auth/03-storage/front/src/core/api/api.helpers.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const setHeader = (header: string, value: string) => {
   axios.defaults.headers.common[header] = value;
-  sessionStorage.setItem(header, value);
+  localStorage.setItem(header, value);
 };
 
 export const restoreHeader = (header: string) => {
-  const value = sessionStorage.getItem(header);
+  const value = localStorage.getItem(header);
   if (value) {
     axios.defaults.headers.common[header] = value;
   }

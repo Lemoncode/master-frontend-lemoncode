376ea0db54e0e31810d1bb145cbed2beba170106
paulmelero <paul.melero@backmarket.com>
2025-03-13 22:47:38 +0100

    Mar 2025: Dia 2

---
+++ 04-frameworks/03-vue/Dia_2- todolist/src/components/ToDoForm.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useTodosStore } from '../stores/todos'
+
+const todosStore = useTodosStore()
+
+const todoInput = ref('')
+
+const addTodo = () => {
+  todosStore.addTodo(todoInput.value)
+  todoInput.value = ''
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <label for="todo">Todo</label>
+    <input id="todo" type="text" v-model="todoInput" class="border-2" />
+    <button>Add</button>
+  </form>
+</template>
---
+++ 04-frameworks/03-vue/Dia_2- todolist/src/types/index.ts
@@ -0,0 +1,5 @@
+export type ToDo = {
+  timestamp: number
+  text: string
+  completed: boolean
+}

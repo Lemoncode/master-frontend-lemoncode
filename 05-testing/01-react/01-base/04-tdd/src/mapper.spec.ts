41b2ed6a4d6bb39cc33a766f5eb2c9127cb006e8
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-04-24 21:24:06 +0200

    update 04

--- 05-testing/01-react/01-base/04-tdd/src/mapper.spec.ts
+++ 05-testing/01-react/01-base/04-tdd/src/mapper.spec.ts
@@ -3,21 +3,19 @@ import * as viewModel from './view-model';
 import { mapMemberListFromApiToVm } from './mapper';
 
 describe('mapper specs', () => {
-  it.each<{
-    members: apiModel.Member[];
-  }>([{ members: undefined }, { members: null }, { members: [] }])(
-    'should return empty array when it feeds members equals $members',
-    ({ members }) => {
-      // Arrange
+  it.each<{ members: apiModel.Member[] }>([
+    { members: undefined },
+    { members: null },
+    { members: [] },
+  ])('should return empty array when it feeds $members', ({ members }) => {
+    // Arrange
 
-      // Act
-      const result = mapMemberListFromApiToVm(members);
+    // Act
+    const result: viewModel.Member[] = mapMemberListFromApiToVm(members);
 
-      // Assert
-      const expectedResult: viewModel.Member[] = [];
-      expect(result).toEqual(expectedResult);
-    }
-  );
+    // Assert
+    expect(result).toEqual([]);
+  });
 
   it('should return array one mapped item when it feed array with one item', () => {
     // Arrange

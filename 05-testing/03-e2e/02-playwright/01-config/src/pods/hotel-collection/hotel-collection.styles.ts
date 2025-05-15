99017c7629d836d3a6032ff1fe6708cb1b9c080c
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-05-15 20:07:03 +0200

    update playwright

--- 05-testing/03-e2e/02-playwright/01-config/src/pods/hotel-collection/hotel-collection.styles.ts
+++ 05-testing/03-e2e/02-playwright/01-config/src/pods/hotel-collection/hotel-collection.styles.ts
@@ -3,18 +3,10 @@ import { theme } from '#core/theme';
 
 export const root = css`
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  grid-row-gap: 2rem;
-  grid-column-gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 2rem;
   list-style: none;
-  margin: 0;
+  max-width: ${theme.breakpoints.values.lg}px;
+  margin: 0 auto;
   padding: 0;
-
-  @media (min-width: ${theme.breakpoints.values.sm}px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @media (min-width: ${theme.breakpoints.values.md}px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
 `;

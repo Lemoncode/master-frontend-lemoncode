2f840dce22857c81cff43492145bdd1403f91f40
Miguel Angel Chimali Cobano <MIGUEL.CHIMALI_COBANO@CONTRACTORS.ROCHE.COM>
2025-09-01 23:07:10 +0200

    refactor react 19 - vite  - basic app

--- 04-frameworks/01-react/03-react-hooks/00-boilerplate/src/index.ts
+++
@@ -1,1 +0,0 @@
-console.log("Vite-boiler-plate");
--- 04-frameworks/01-react/03-react-hooks/00-boilerplate/src/index.tsx
+++ 04-frameworks/01-react/03-react-hooks/00-boilerplate/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
-import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);

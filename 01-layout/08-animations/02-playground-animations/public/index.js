02eb84cd911fb05ac2e45684bd15abdfc736dc4a
Javier Calzado <javi.calzado@lemoncode.net>
2025-10-31 14:18:16 +0100

    gallery flip sample

--- 01-layout/08-animations/02-playground-animations/public/index.js
+++ 01-layout/08-animations/02-playground-animations/public/index.js
@@ -9,7 +9,10 @@ console.log("This is your playground, experiment with native CSS animations!");
 
 const sectionDefinition = [
   { title: "Transform", cardNames: ["Basic transform", "3D transform"] },
-  { title: "Transition", cardNames: ["Basic transition", "CSS Jitter", "List pick"] },
+  {
+    title: "Transition",
+    cardNames: ["Basic transition", "CSS Jitter", "List pick", "Gallery flip"],
+  },
   { title: "Animations", cardNames: ["List scroll"] },
 ];
 

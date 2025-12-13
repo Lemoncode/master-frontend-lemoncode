4cecdb88d3a1d9e2e6c7fc44e41a11fd0bb0dae5
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-12-13 15:04:14 +0100

    update 04-static-site-generation

--- 04-frameworks/08-nextjs/04-static-site-generation/src/app/cars/layout.tsx
+++ 04-frameworks/08-nextjs/04-static-site-generation/src/app/cars/layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 import classes from './layout.module.css';
 
 interface Props {

eddb4c97a17a764948e0554f385274742bbfc414
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-12-16 12:45:08 +0100

    add 03-boilerplate

---
+++ 04-frameworks/17-tanstack-start/04-tanstack-start-migration/src/pods/car/api/car.api.ts
@@ -0,0 +1,16 @@
+import { ENV } from '#core/constants';
+import { Car } from './car.api-model';
+
+const url = `${ENV.BASE_API_URL}/cars`;
+
+export const getCar = async (id: string): Promise<Car> =>
+  await fetch(`${url}/${id}`).then((response) => response.json());
+
+export const bookCar = async (car: Car): Promise<boolean> => {
+  await fetch(`${url}/${car.id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(car),
+  });
+  return true;
+};
---
+++ 04-frameworks/17-tanstack-start/04-tanstack-start-migration/src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import * as React from 'react';
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <div>Hello "__root"!</div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </React.Fragment>
+  );
+}

bd7830fa3e7ceb0b9f0c989d64bb71db76ce96ae
daniel.sanchez <daniel.sanchez@lemoncode.net>
2025-12-18 11:57:09 +0100

    copy previous example

--- 04-frameworks/17-tanstack-start/05-api-routes/src/core/constants/env.constants.ts
+++ 04-frameworks/17-tanstack-start/05-api-routes/src/core/constants/env.constants.ts
@@ -1,8 +1,5 @@
 export const ENV = {
-  BASE_API_URL:
-    process.env.PUBLIC_BASE_API_URL ||
-    import.meta.env.PUBLIC_BASE_API_URL ||
-    '',
+  BASE_API_URL: process.env.BASE_API_URL || '',
   BASE_PICTURES_URL:
     process.env.PUBLIC_BASE_PICTURES_URL ||
     import.meta.env.PUBLIC_BASE_PICTURES_URL ||
